0694ace645132386de5b950b09c2eb6a26773a2e
Sudipto Mondal <smondal201184@bscse.uiu.ac.bd>
2022-09-20 21:35:04 +0600

    patient home page design created

---
+++ app/src/main/java/com/example/easynurse/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.easynurse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    //Array of titles
+    String[] appTitleArray = {"Appointment 1", "Appointment 2", "Appointment 3", "Appointment 4", "Appointment 5", "Appointment 6", "Appointment 7", "Appointment 8", "Appointment 9"};
+
+
+    ListView titleList;
+    ArrayAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        adapter = new ArrayAdapter<>(getApplicationContext(), R.layout.appointment_list_layout, R.id.card_text, appTitleArray);
+        titleList = findViewById(R.id.app_title_list);
+        titleList.setAdapter(adapter);
+
+
+
+
+
+    }
+}
