7ae9f3dfdc104367d69ffd8637866b2e4960ac91
Sudipto Mondal <smondal201184@bscse.uiu.ac.bd>
2022-09-30 20:32:13 +0600

    Job details show

---
+++ app/src/main/java/com/example/easynurse/AllDetailsAboutJob.java
@@ -0,0 +1,14 @@
+package com.example.easynurse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AllDetailsAboutJob extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_details_about_job);
+    }
+}
