3b70f2dc876f5bddd4833af5813ec5a80a489ede
Sudipto Mondal <smondal201184@bscse.uiu.ac.bd>
2022-09-30 21:28:21 +0600

    Dark mode in Settings

---
+++ app/src/main/java/com/example/easynurse/Settings.java
@@ -0,0 +1,37 @@
+package com.example.easynurse;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+public class Settings extends AppCompatActivity {
+
+
+    Switch aSwitch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        getSupportActionBar().hide();
+        aSwitch = findViewById(R.id.darkModeSwitch);
+
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b)
+                {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                }
+                else
+                {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+            }
+        });
+
+    }
+}
