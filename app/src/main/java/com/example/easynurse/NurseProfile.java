c58ddb5384860ca8ed39f4edca4022a43ef21d91
Sudipto Mondal <smondal201184@bscse.uiu.ac.bd>
2022-09-30 18:58:08 +0600

    Nurse home & patient, nurse toolbar

---
+++ app/src/main/java/com/example/easynurse/NurseProfile.java
@@ -0,0 +1,14 @@
+package com.example.easynurse;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NurseProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nurse_profile);
+    }
+}
