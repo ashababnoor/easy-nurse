3a21881638c9bf06b81a264c77085938645cd36b
Debopom Sutradhar <dsutradhar201046@bscse.uiu.ac.bd>
2022-09-30 12:02:41 +0600

    bug fixed

--- app/src/main/java/com/example/easynurse/JobPostingForm.java
+++ app/src/main/java/com/example/easynurse/JobPostingForm.java
@@ -13,6 +13,8 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ValueEventListener;
 
 public class JobPostingForm extends AppCompatActivity {
