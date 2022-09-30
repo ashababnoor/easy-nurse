cb731b4975325b5775a495b700f77b37487931d3
Shabab <ashababnoor@gmail.com>
2022-09-30 21:26:48 +0600

    login via fetching data from firebase

--- app/src/main/java/com/example/easynurse/MainActivity.java
+++ app/src/main/java/com/example/easynurse/MainActivity.java
@@ -1,19 +1,33 @@
 package com.example.easynurse;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_goto_signup;
+    private Button btn_goto_signup;
+    private Button btn_login;
+    private EditText et_login_email;
+    private EditText et_login_pass;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +35,65 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        et_login_email = (EditText) findViewById(R.id.et_login_email);
+        et_login_pass = (EditText) findViewById(R.id.et_login_pass);
+        btn_login = (Button) findViewById(R.id.btn_login);
         btn_goto_signup = (Button) findViewById(R.id.btn_goto_signup);
+        progressBar = (ProgressBar) findViewById(R.id.pb_login);
 
         btn_goto_signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent changeToSignup = new Intent(MainActivity.this, RegisterUser.class);
                 MainActivity.this.startActivity(changeToSignup);
+            }
+        });
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = et_login_email.getText().toString().trim();
+                String pass = et_login_pass.getText().toString().trim();
+
+                if (email.isEmpty()){
+                    et_login_email.setError("Email is required!");
+                    et_login_email.requestFocus();
+                    return;
+                }
+
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    et_login_email.setError("Please provide a valid email!");
+                    et_login_email.requestFocus();
+                    return;
+                }
+
+                if (pass.isEmpty()){
+                    et_login_pass.setError("Password is required");
+                    et_login_pass.requestFocus();
+                    return;
+                }
+
+                if (pass.length() < 8){
+                    et_login_pass.setError("Password must be at least 8 characters!");
+                    et_login_pass.requestFocus();
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+                mAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "Logged in successfully!", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "Failed to log in. Please check your email and password.", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+
             }
         });
     }
--- app/src/main/java/com/example/easynurse/RegisterFragmentInitialForm.java
+++ app/src/main/java/com/example/easynurse/RegisterFragmentInitialForm.java
@@ -55,46 +55,6 @@ public class RegisterFragmentInitialForm extends Fragment {
 
     final Calendar myCalendar = Calendar.getInstance();
 
-//    // TODO: Rename parameter arguments, choose names that match
-//    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//    private static final String ARG_PARAM1 = "param1";
-//    private static final String ARG_PARAM2 = "param2";
-//
-//    // TODO: Rename and change types of parameters
-//    private String mParam1;
-//    private String mParam2;
-//
-//    public RegisterFragmentInitialForm() {
-//        // Required empty public constructor
-//    }
-//
-//    /**
-//     * Use this factory method to create a new instance of
-//     * this fragment using the provided parameters.
-//     *
-//     * @param param1 Parameter 1.
-//     * @param param2 Parameter 2.
-//     * @return A new instance of fragment RegisterFragmentInitialForm.
-//     */
-//    // TODO: Rename and change types and number of parameters
-//    public static RegisterFragmentInitialForm newInstance(String param1, String param2) {
-//        RegisterFragmentInitialForm fragment = new RegisterFragmentInitialForm();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
-//
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
-//    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -192,34 +152,6 @@ public class RegisterFragmentInitialForm extends Fragment {
                 fragmentTransaction.replace(R.id.register_fragment, fragment);
                 fragmentTransaction.addToBackStack(null);
                 fragmentTransaction.commit();
-
-//                progressBar.setVisibility(View.VISIBLE);
-//                mAuth.createUserWithEmailAndPassword(email, pass)
-//                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-//                            @Override
-//                            public void onComplete(@NonNull Task<AuthResult> task) {
-//                                if (task.isSuccessful()){
-//                                    User user = new User(name, email, pass, dob, phone, finalGender);
-//
-//                                    FirebaseDatabase.getInstance().getReference("Users")
-//                                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-//                                            .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
-//                                                @Override
-//                                                public void onComplete(@NonNull Task<Void> task) {
-//                                                    if (task.isSuccessful()){
-//                                                        Toast.makeText(getActivity(), "User has been registered successfully", Toast.LENGTH_LONG).show();
-//                                                        progressBar.setVisibility(View.GONE);
-//                                                        registerViewToMainActivityView();
-//                                                    }
-//                                                    else {
-//                                                        Toast.makeText(getActivity(), "Failed to register user.", Toast.LENGTH_LONG).show();
-//                                                        progressBar.setVisibility(View.GONE);
-//                                                    }
-//                                                }
-//                                            });
-//                                }
-//                            }
-//                        });
             }
         });
 
@@ -266,4 +198,5 @@ public class RegisterFragmentInitialForm extends Fragment {
         getActivity().startActivity(changeToLogin);
         getActivity().finish();
     }
+
 }
